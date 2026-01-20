43b33cb9c259ae79eb2be06e8672fb4da815cdf5
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2026-01-20 20:43:16 +0500

    First Commit

---
+++ Weather.py
@@ -0,0 +1,56 @@
+from flask import Flask,render_template,request
+from dotenv import load_dotenv
+import os
+import requests
+ 
+#Loading the API from the env file ---> make sure to add youe API key in the 
+#.env file in the variable OPENWEATHER_API_KEY to make the app work !!!
+load_dotenv()
+WEATHER_API_KEY=os.getenv("OPENWEATHER_API_KEY")
+IMAGES_API_KEY=os.getenv("PIXABAY_API_KEY")
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    city = request.form.get("city")
+
+    #WEATHER !
+
+    weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric"
+    response = requests.get(weather_url)
+    if response.status_code==200:    
+        weather_data = response.json()
+    else:
+        weather_data="Invalid City"
+
+
+
+    #IMAGE
+    if weather_data=="Invalid City":
+        city="sarcastic+funny+fail"
+    else:
+        city=city
+        
+    pixabay_url= f"https://pixabay.com/api/?key={IMAGES_API_KEY}&q={city}+landmark&image_type=photo&per_page=5"
+    pixabay_data = requests.get(pixabay_url).json()
+
+    city_image = ""
+    if pixabay_data["hits"]:
+        # pick a random landmark from top 5 results
+        import random
+        city_image= random.choice(pixabay_data["hits"])["largeImageURL"]
+    print(city_image) 
+
+    return render_template("index.html", weather=weather_data,city_image=city_image)
+
+
+if __name__=="__main__":
+    app.run(debug=False,port=8000)
---
+++ readme.md
@@ -0,0 +1,32 @@
+## Flask Weather App
+
+This is a simple and minimal weather-based web application built using Flask.  
+The project uses Bootstrap for basic styling, Jinja2 for templating, and standard HTML/CSS along with core Python concepts.
+
+The application fetches real-time weather data using the OpenWeather API and dynamically displays city-related background images using the Pixabay API.
+
+---
+
+## Tech Stack
+
+- Flask  
+- Jinja2  
+- Bootstrap  
+- HTML  
+- CSS  
+- Python  
+- OpenWeather API  
+- Pixabay API  
+
+---
+
+## Environment Setup
+
+Create a `.env` file in the root directory of the project and add your own API keys as shown below:
+
+```env
+OPENWEATHER_API_KEY=your_openweather_api_key_here
+PIXABAY_API_KEY=your_pixabay_api_key_here
+
+Feel free to share suggestions or improvements at:
+sp25-bse-118@cuilahore.edu.pk
